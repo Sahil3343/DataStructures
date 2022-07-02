d4cc8b83ab70fd87d8e756e80d738dec19c519e3
Sahil Mahajan <sahil3343@gmail.com>
2022-07-02 19:01:24 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ import java.util.ArrayList;
 
 public class Main {
 
-    static HashTables hashTables = new HashTables();
-
     public static void main(String[] args) {
-        hashTables.HashTableImplementation();
+
     }
 }
